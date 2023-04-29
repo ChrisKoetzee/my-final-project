c261586ce9344f58e49cdb6e4253e604ddf8642f
ChrisKoetzee <102851549+ChrisKoetzee@users.noreply.github.com>
2023-04-29 14:22:43 +0200

    deleting DB info

--- server/utils/config.js
+++ server/utils/config.js
@@ -11,11 +11,11 @@ function createDatabaseUrl() {
 	if (process.env.DATABASE_URL) {
 		return process.env.DATABASE_URL;
 	}
-	const host = process.env.DB_HOST ?? "dpg-cgt7t1o2qv2fdefmib5g-a.oregon-postgres.render.com";
-	const name = process.env.DB_NAME ?? "ezit";
-	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "LUGko6djTy3ET7Dj5V86e74I3paNqTFR";
+	const host = process.env.DB_HOST ?? "";
+	const name = process.env.DB_NAME ?? "";
+	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
 	const port = process.env.DB_PORT ?? "5432";
-	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "ezit";
+	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
 	const userinfo = `${username}:${password}`;
 	return `postgres://${
 		userinfo !== ":" ? `${userinfo}@` : ""
