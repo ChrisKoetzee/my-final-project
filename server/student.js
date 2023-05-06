fc209c7de8dc5ef06f000eaf19b26f360d98656e
ChrisKoetzee <102851549+ChrisKoetzee@users.noreply.github.com>
2023-05-06 11:10:20 +0200

    adding gender and fixing error

--- server/student.js
+++ server/student.js
@@ -6,28 +6,28 @@ const router = Router();
 
 router.post('/table', (req, res) => {
 	query
-		.query('CREATE TABLE IF NOT EXISTS students (id serial PRIMARY KEY, firstName VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, dateOfBirth DATE NOT NULL, email VARCHAR(150) NOT NULL, phoneNumber INTEGER  NOT NULL, passwordStudent VARCHAR (10))')
+		.query('CREATE TABLE IF NOT EXISTS students (id serial PRIMARY KEY, fullNames VARCHAR(50) NOT NULL, surname VARCHAR(50) NOT NULL, dateOfBirth DATE NOT NULL, email VARCHAR(150) NOT NULL, phoneNumber INTEGER  NOT NULL, gender VARCHAR(6) passwordStudent VARCHAR (10))')
 		.then((result) => res.status(200).json(result.command))
 		.catch((error) => console.error(error));
 });
 router.post('/new', (req, res) =>{
-	const {firstName, lastName, dateOfBirth, email, phoneNumber, password} = req.body;
-	const Query = 'INSERT INTO students (firstName, lastName, dateOfBirth, email, phoneNumber, passwordStudent ) VALUES($1, $2, $3, $4, $5, $6)'
+	const {fullNames, surname, dateOfBirth, email, phoneNumber, gender, password} = req.body;
+	const Query = 'INSERT INTO students (fullNames, surname, dateOfBirth, email, phoneNumber, gender, passwordStudent ) VALUES($1, $2, $3, $4, $5, $6)'
 	query
-		.query(Query, [firstName, lastName, dateOfBirth, email, phoneNumber, password])
+		.query(Query, [fullNames, surname, dateOfBirth, email, phoneNumber, gender, password])
 		.then(() => res.status(200).json({message: 'Student loaded successfully'}))
-		.catch((error) => res.status(500).json({error: 'Information loaded incorrectly - ' + error.message}));
+		.catch((error) => res.status(400).json({error: 'Information loaded incorrectly - ' + error.message}));
 });
 router.get('/', (req, res) => {
 	query
-        .query('SELECT id, firstName, lastName, dateOfBirth, email, phoneNumber FROM students')
+        .query('SELECT id, fullNames, surname, dateOfBirth, email, gender, phoneNumber FROM students')
 	  	.then((result) => res.json(result.rows))
 		.catch((error) => res.status(500).json(error));
 });
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
 	query
-        .query('SELECT firstName, lastName, dateOfBirth, email, phoneNumber FROM students WHERE id=$1', [id])
+        .query('SELECT fullNames, surname, dateOfBirth, email, gender, phoneNumber FROM students WHERE id=$1', [id])
 	  	.then((result) => res.json(result.rows))
 		.catch((error) => res.status(500).json(error));
 });
@@ -48,9 +48,9 @@ router.delete('/:id', (req, res) => {
 })
 router.put('/:id', (req, res) => {
 	const id = req.params.id;
-	const {firstName, lastName, dateOfBirth, email, phoneNumber} = req.body;
+	const {fullNames, surname, dateOfBirth, email, phoneNumber} = req.body;
 	query 
-		.query('UPDATE students SET firstName=? lastName=? dateOfBirth=?, email=?, phoneNumber = ? WHERE id=$1', [id, firstName, lastName, dateOfBirth, email, phoneNumber])
+		.query('UPDATE students SET fullNames=? surname=? dateOfBirth=?, email=?, phoneNumber=? gender=? WHERE id=$1', [id, fullNames, surname, dateOfBirth, email, phoneNumber, gender])
 		.then(() => {
 			query.query('COMMIT', (err) => {
 				if (err){
