323fcdd53e858b24efa496700f829fbf6f1f3789
Diego Harera <mydiego27@gmail.com>
2023-05-11 21:45:26 +0200

    Navabsr with black text

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -1,77 +1,107 @@
 import React, { useEffect, useRef } from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
 
+
 function NavBar() {
-  const headerRef = useRef(null);
-  const menuRef = useRef(null);
+	const headerRef = useRef(null);
+	const menuRef = useRef(null);
 
-  const stickyHeaderFunc = () => {
-    window.addEventListener("scroll", () => {
-      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        headerRef.current.classList.add("sticky_header");
-      } else {
-        headerRef.current.classList.remove("sticky_header");
-      }
-    });
-  };
+	const stickyHeaderFunc = () => {
+		window.addEventListener("scroll", () => {
+			if (
+				document.body.scrollTop > 80 ||
+				document.documentElement.scrollTop > 80
+			) {
+				headerRef.current.classList.add("sticky_header");
+			} else {
+				headerRef.current.classList.remove("sticky_header");
+			}
+		});
+	};
 
-  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+	const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
 
-  useEffect(() => {
-    stickyHeaderFunc();
+	useEffect(() => {
+		stickyHeaderFunc();
 
-    return () => window.removeEventListener("scroll", stickyHeaderFunc);
-  }, []);
+		return () => window.removeEventListener("scroll", stickyHeaderFunc);
+	}, []);
 
-  return (
-    <header ref={headerRef} className="w-full h-80px d-flex align-items-center">
-      <div className="container">
-        <div className="d-flex justify-content-between">
-          {/* Logo */}
-          <div className="d-flex align-items-center gap-3">
-            <div className="leading-13px">
-              <h2 className="text-xl font-weight-bold">Douglas School</h2>
-            </div>
-          </div>
-          {/* Logo end */}
+	return (
+		<header ref={headerRef} className="w-full h-80px d-flex align-items-center">
+			<div className="container">
+				<div className="d-flex justify-content-between">
+					{/* Logo */}
+					<div className="d-flex align-items-center gap-3">
+						<div className="leading-13px">
+							<h2 className="text-xl font-weight-bold">Douglas School</h2>
+						</div>
+					</div>
+					{/* Logo end */}
 
-          {/* Menu Start */}
-          <div ref={menuRef} className="menu">
-            <Navbar expand="md" className="p-0">
-              <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                  <Nav.Link className="text-smallTextColor font-weight-600" href="#about">
-                    School Info
-                  </Nav.Link>
-                  <Nav.Link className="text-smallTextColor font-weight-600" href="#services">
-                    Student Info
-                  </Nav.Link>
-                  <Nav.Link className="text-smallTextColor font-weight-600" href="#portfolio">
-                    NewsLetter
-                  </Nav.Link>
-                  <Nav.Link className="text-smallTextColor font-weight-600" href="#contact">
-                    Donation
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </div>
-          {/* Menu End */}
+					{/* Menu Start */}
+					<div ref={menuRef} className="menu">
+						<Navbar expand="md" className="p-0">
+							<Navbar.Toggle
+								aria-controls="basic-navbar-nav"
+								className="border-0"
+							/>
+							<Navbar.Collapse id="basic-navbar-nav">
+								<Nav className="ml-auto">
+									<Nav.Link
+										className="text-black font-weight-600"
+										href="#"
+									>
+										School Info
+									</Nav.Link>
+									<Nav.Link
+										className="text-black font-weight-600"
+										href="#"
+									>
+										Student Info
+									</Nav.Link>
+									<Nav.Link
+										className="text-black font-weight-600"
+										href="#"
+									>
+										NewsLetter
+									</Nav.Link>
+									<Nav.Link
+										className="text-black font-weight-600"
+										href="#"
+									>
+										Donation
+									</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>
+					</div>
+					{/* Menu End */}
 
-          {/* Menu Right */}
-          <div className="d-flex align-items-center gap-4">
-            <Button className="d-flex align-items-center gap-2 text-smallTextColor font-weight-600 border py-2 px-4 rounded-8px max-h-40px hover:bg-smallTextColor hover:text-white ease-in duration-300">
-              <i className="ri-send-plane-fill"></i> Login As Student
-            </Button>
-            <span onClick={toggleMenu} className="text-2xl text-smallTextColor d-md-none cursor-pointer">
-              <i className="ri-menu-line"></i>
-            </span>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+					{/* Menu Right */}
+					<div className="d-flex align-items-center gap-4">
+					<Button
+						className="d-flex align-items-center gap-2 font-weight-600 border py-2 px-4 rounded-8px max-h-40px hover:bg-smallTextColor hover:text-white"
+						style={{
+							transition: "background-color 0.3s ease-in, color 0.3s ease-in",
+							color: "black",
+							borderColor: "black",
+							backgroundColor: "white",
+						}}
+						>
+						Login As Student
+						</Button>
+						<span
+							onClick={toggleMenu}
+							className="text-2xl text-smallTextColor d-md-none cursor-pointer"
+						>
+							<i className="ri-menu-line"></i>
+						</span>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default NavBar;
--- server/api.js
+++ server/api.js
@@ -1,47 +1,47 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
 import logger from "./utils/logger";
-const JWT_SECRET = "Super Secret code....";
+// const JWT_SECRET = "Super Secret code....";
 // Demo Database
-const Users = [
-	{
-		id: 1,
-		name: "Diego Harera",
-		email: "mydiego@gmail.com",
-		password: "Zxcvbnm2023",
-	},
-	{
-		id: 2,
-		name: "John Due",
-		email: "johndue@gmail.com",
-		password: "Qwerty2023",
-	},
-];
 
 const router = Router();
 
 router.get("/", (_, res) => {
+	const Users = [
+		{
+			id: 1,
+			name: "Diego Harera",
+			email: "mydiego@gmail.com",
+			password: "Zxcvbnm2023",
+		},
+		{
+			id: 2,
+			name: "John Due",
+			email: "johndue@gmail.com",
+			password: "Qwerty2023",
+		},
+	];
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
 });
 
-router.post("/login", (req, res) => {
-	const { email, password } = req.body;
+// router.post("/login", (req, res) => {
+// 	const { email, password } = req.body;
 
-	const user = Users.find((user) => user.email === email);
+// 	const user = Users.find((user) => user.email === email);
 
-	if (!user) {
-		return res.status(400).json({ error: "Invalid credentials" });
-	}
+// 	if (!user) {
+// 		return res.status(400).json({ error: "Invalid credentials" });
+// 	}
 
-	if (user.password !== password) {
-		return res.status(400).json({ error: "Invalid credentials" });
-	}
+// 	if (user.password !== password) {
+// 		return res.status(400).json({ error: "Invalid credentials" });
+// 	}
 
-	const payload = { id: user.id };
-	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "30m" });
-	res.json({ token });
-});
+// 	const payload = { id: user.id };
+// 	const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "30m" });
+// 	res.json({ token });
+// });//*css*/`
 
 export default router;
