f3c4d76d9ce6c4283466df4e3344ba93d5143a9a
shafiekdavids <shafiekd@gmail.com>
2023-05-14 12:20:58 +0200

    Removed Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Footer from "./pages/Footer";
 import TeacherForm from "./pages/TeacherForm";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
-		<Footer />
 		<Route path="/admin/teacher-registration" element={<TeacherForm />} />
 	</Routes>
 );
