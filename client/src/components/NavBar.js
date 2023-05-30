37a48b2ead0c6199f0b95a43efbf636305d4a6ea
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-05-30 20:48:25 +0200

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -56,10 +56,8 @@ function NavBar() {
 									<Nav className="mr-auto">
 										<Nav.Link
 											className="text-black font-weight-600"
-
 											as={Link}
-											to="/about/this/site"
-											
+											to="/about/this/site"	
 										>
 											School Info
 										</Nav.Link>
